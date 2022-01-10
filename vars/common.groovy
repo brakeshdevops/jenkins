3d7eadcfd06d9fa804d057e8869f7978c2bff7ed
brakesh <bandarupallirakesh@gmail.com>
2022-01-10 20:43:15 +0530

    Shared library updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,4 +1,4 @@
 def sonarQube()
 {
-    sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://3.235.28.135 :9000 -Dsonar.login=admin -Dsonar.password=admin123'
+    sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://3.235.28.135:9000 -Dsonar.login=admin -Dsonar.password=admin123'
 }
