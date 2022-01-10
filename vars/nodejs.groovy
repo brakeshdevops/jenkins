28b58f481fe397bac94893d0766186ffbc51de90
brakesh <bandarupallirakesh@gmail.com>
2022-01-10 09:18:06 +0530

    Shared library updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,8 +1,38 @@
-def info(message)
+def call()
 {
-    echo "Information: ${message}"
-}
-def warning(message)
-{
-    echo "Warning: ${message}"
+    pipeline
+            {
+                agent
+                        {
+                            label 'WORKSTATION'
+                        }
+                triggers
+                        {
+                            pollSCM('*/2 * * * *')
+                        }
+                stages
+                        {
+                            stage('Compile the code')
+                                    {
+                                        steps
+                                                {
+                                                   sh 'echo compile the code'
+                                                }
+                                    }
+                            stage('Check the code quality')
+                                    {
+                                        steps
+                                                {
+                                                    sh 'echo check the code quality'
+                                                }
+                                    }
+                            stage('Test Cases')
+                                    {
+                                        steps
+                                                {
+                                                    sh 'echo test cases'
+                                                }
+                                    }
+                        }
+            }
 }
