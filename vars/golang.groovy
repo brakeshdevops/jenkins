f708cbcc958ae8a78be098d7dfa2d261c71aea3e
brakesh <bandarupallirakesh@gmail.com>
2022-01-11 12:35:54 +0530

    Shared library updated

---
+++ vars/golang.groovy
@@ -0,0 +1,45 @@
+def call()
+{
+    pipeline
+            {
+                agent
+                        {
+                            label "${BUILD_LABEL}"
+                        }
+                triggers
+                        {
+                            pollSCM('*/2 * * * *')
+                        }
+                stages
+                        {
+                            stage('Check the code quality')
+                                    {
+                                        steps
+                                                {
+                                                    sh 'echo check the ${COMPONENT} code quality'
+                                                }
+                                    }
+                            stage('Lint Checks')
+                                    {
+                                        steps
+                                                {
+                                                    sh 'echo Lint Checks'
+                                                }
+                                    }
+                            stage('Test Cases')
+                                    {
+                                        steps
+                                                {
+                                                    sh 'echo test cases'
+                                                }
+                                    }
+                        }
+                post
+                        {
+                            always
+                                    {
+                                        cleanWs()
+                                    }
+                        }
+            }
+}
