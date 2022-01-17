25f013b7f8871f4d5eebf408d6a5c1cc17cfa678
brakesh <bandarupallirakesh@gmail.com>
2022-01-17 16:00:56 +0530

    Shared library updated

--- seed.groovy
+++ seed.groovy
@@ -49,3 +49,27 @@ for(i in 0..size)
         }
     }
 }
+folder('Mutable') {
+    displayName('Mutable')
+    description('Mutable')
+}
+pipelineJob('Mutable') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/brakeshdevops/terraform-vpc.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
