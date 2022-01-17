322b5e824897a0d60446a1a49190410eb6e5c184
brakesh <bandarupallirakesh@gmail.com>
2022-01-17 17:28:22 +0530

    Shared library updated

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -29,6 +29,9 @@ def call()
           steps
           {
             sh '''
+                sudo yum install -y yum-utils
+                sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+                sudo yum -y install terraform
                 terraform init
             '''
           }
